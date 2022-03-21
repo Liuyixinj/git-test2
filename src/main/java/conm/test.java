76bafb4ef53b089e80751eaa28f53f94e7ba097d
Liuyx <1197717246@qq.com>
2022-03-21 21:46:08 +0800

    master-1

---
+++ src/main/java/conm/test.java
@@ -0,0 +1,13 @@
+package conm;
+
+/**
+ * @author 刘怡鑫
+ * @version 1.0
+ * @description: TODO
+ * @date 2022/3/21 21:43
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("master-1");
+    }
+}
